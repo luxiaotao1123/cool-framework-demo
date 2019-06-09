cc0afc679db9c0e701939f7280407388162b7d55
vincent <1341870251@qq.com>
2019-06-09 22:36:57 +0800

    #

--- src/main/java/com/cool/demo/DemoApplication.java
+++ src/main/java/com/cool/demo/DemoApplication.java
@@ -1,24 +1,26 @@
 package com.cool.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cool.demo.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+//    @Autowired
+//    private UserService userService;
+//
+//
 //    @GetMapping("/test")
 //    @ResponseBody
 //    public String test(){
-//        User user = new User();
-//        user.setAge(18);
-//        user.setName("陆晓涛");
-//        long save = userService.save(user);
-//        return String.valueOf(save);
+//        User user = userService.selectById("1136299639260106754");
+//        return user.toString();
 //    }
-
 
 }
--- src/main/java/com/cool/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.cool.demo.service;
-
-import com.core.service.SqlBaseService;
-import com.cool.demo.entity.User;
-
-public interface UserService extends SqlBaseService<User> {
-
-}
