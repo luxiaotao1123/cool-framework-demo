cddd99ae9bec401ff8904273fa135fbe9c7c2dc8
vincent <1341870251@qq.com>
2019-06-12 09:46:28 +0800

    #

---
+++ src/main/java/com/cool/demo/common/AuthController.java
@@ -0,0 +1,34 @@
+package com.cool.demo.common;
+
+import com.core.common.R;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by vincent on 2019-06-11
+ */
+@Controller
+public class AuthController {
+
+    @RequestMapping("/auth")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/login.action")
+    @ResponseBody
+    public R loginAction(String username, String password){
+        if (username.equals("admin") && password.equals("admin")){
+            return R.ok("token123");
+        }else {
+            return R.error("密码错误");
+        }
+    }
+
+}
