c9355263d021b853ce43ff8349961ca362ed8cb8
vincent <1341870251@qq.com>
2019-06-24 10:04:45 +0800

    #

--- src/main/java/com/cool/demo/system/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.cool.demo.system.service;
-
-import com.cool.demo.system.entity.User;
-import com.baomidou.mybatisplus.service.IService;
-
-public interface UserService extends IService<User> {
-
-}
