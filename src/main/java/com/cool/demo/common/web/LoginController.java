ed8fbd986628451630a68b894a7a5a011d1a3805
vincent <1341870251@qq.com>
2019-09-09 14:19:43 +0800

    1.3

--- src/main/java/com/cool/demo/common/web/LoginController.java
+++ src/main/java/com/cool/demo/common/web/LoginController.java
@@ -3,14 +3,17 @@ package com.cool.demo.common.web;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.cool.demo.common.CodeRes;
 import com.cool.demo.system.entity.Resource;
+import com.cool.demo.system.entity.RoleResource;
 import com.cool.demo.system.entity.User;
 import com.cool.demo.system.entity.UserLogin;
 import com.cool.demo.system.service.ResourceService;
+import com.cool.demo.system.service.RoleResourceService;
 import com.cool.demo.system.service.UserLoginService;
 import com.cool.demo.system.service.UserService;
 import com.core.common.Cools;
 import com.core.common.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,8 @@ public class LoginController {
     private UserLoginService userLoginService;
     @Autowired
     private ResourceService resourceService;
+    @Autowired
+    private RoleResourceService roleResourceService;
 
     @RequestMapping("/login.action")
     public R loginAction(String mobile, String password){
@@ -86,14 +91,15 @@ public class LoginController {
         return R.ok(result);
     }
 
-    @RequestMapping("/power/auth")
-    public R power(){
+    @RequestMapping("/power/list/auth")
+    public R powerList(){
         List<Resource> oneLevels = resourceService.selectList(new EntityWrapper<Resource>().eq("level", 1).eq("status", 1));
         List<Map> result = new ArrayList<>();
         for (Resource oneLevel : oneLevels){
             Map<String, Object> oneLevelMap = new HashMap<>();
             oneLevelMap.put("title", oneLevel.getName());
             oneLevelMap.put("id", oneLevel.getCode());
+            oneLevelMap.put("spread", true);
             List<Resource> twoLevels = resourceService.selectList(new EntityWrapper<Resource>().eq("pcode", oneLevel.getCode()).eq("status", 1));
             List<Map> twoLevelsList = new ArrayList<>();
             oneLevelMap.put("children", twoLevelsList);
@@ -101,11 +107,38 @@ public class LoginController {
                 Map<String, Object> twoLevelMap = new HashMap<>();
                 twoLevelMap.put("title", twoLevel.getName());
                 twoLevelMap.put("id", twoLevel.getCode());
+                oneLevelMap.put("spread", true);
                 twoLevelsList.add(twoLevelMap);
             }
             result.add(oneLevelMap);
         }
         return R.ok(result);
+    }
+
+    @RequestMapping(value = "/power/{roleId}/auth")
+    public R get(@PathVariable("roleId") Long roleId) {
+        List<RoleResource> roleResources = roleResourceService.selectList(new EntityWrapper<RoleResource>().eq("role_id", roleId));
+        List<String> result = new ArrayList<>();
+        for (RoleResource roleResource : roleResources){
+            Resource resource = resourceService.selectById(roleResource.getResourceId());
+            result.add(resource.getCode());
+        }
+        return R.ok(result);
+    }
+
+    @RequestMapping("/power/auth")
+    public R power(Long roleId, String[] powers){
+        roleResourceService.delete(new EntityWrapper<RoleResource>().eq("role_id", roleId));
+        if (!Cools.isEmpty(powers)){
+            for (String power : powers) {
+                Resource resource = resourceService.selectOne(new EntityWrapper<Resource>().eq("code", power));
+                RoleResource roleResource = new RoleResource();
+                roleResource.setRoleId(roleId);
+                roleResource.setResourceId(resource.getId());
+                roleResourceService.insert(roleResource);
+            }
+        }
+        return R.ok();
     }
 
 }
