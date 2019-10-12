cbff703b6637ae7a457c49897440c3f5858fcfb8
vincent <1341870251@qq.com>
2019-10-12 10:25:50 +0800

    1.5

--- src/main/webapp/static/js/common.js
+++ src/main/webapp/static/js/common.js
@@ -3,6 +3,11 @@ var detailHeight = '90%';
 // 详情窗口-宽度
 var detailWidth = '90%';
 
+// 非空判断
+function isEmpty(obj){
+    return typeof obj == "undefined" || obj == null || obj === "";
+}
+
 // 时间 ==>> 字符串
 function dateToStr(date) {
     var time = new Date(date);
@@ -37,4 +42,57 @@ function convertDisabled(el, param) {
     el.each(function () {
         $(this).attr("disabled", param);
     });
+}
+
+/**
+ * 搜索自动补全 -- input
+ */
+function autoLoad(val) {
+    var inputDomVal = document.querySelector("input[data-key="+val+"]").value;
+    var selectDom = document.querySelector("select[data-key="+val+"Select]");
+
+    if (isEmpty(inputDomVal)){
+        return;
+    }
+    selectDom.length = 0;
+    $.ajax({
+        url: "/"+val+"/auth",
+        headers: {'token': localStorage.getItem('token')},
+        data: {condition: inputDomVal},
+        method: 'POST',
+        traditional:true,
+        success: function (res) {
+            if (res.code === 200){
+                var list = res.data;
+                for (var i=0;i<list.length;i++){
+                    var option = new Option(list[i].value+"#"+list[i].id, i);
+                    option.title = list[i].id;
+                    selectDom.options[i] = option;
+                }
+                if(list.length>0){
+                    selectDom.style.display='block';
+                }else{
+                    selectDom.style.display='none';
+                }
+            } else if (res.code === 403){
+                top.location.href = "/";
+            } else {
+                layer.msg(res.msg)
+            }
+        }
+    });
+}
+
+/**
+ * 搜索自动补全 -- select
+ */
+function confirmed(val){
+    var inputDom = $("input[data-key="+val.substring(0,val.length - 6)+"]");
+    var selectDom = $("select[data-key="+val+"]");
+
+    selectDom.dblclick(function(){
+        inputDom.focus();
+        selectDom.css("display","none");
+    });
+    inputDom.val($("option:selected").attr("title"));
 }
