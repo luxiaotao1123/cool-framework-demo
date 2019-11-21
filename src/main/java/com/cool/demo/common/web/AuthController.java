e3d3ab82bb7a12463a111bb64d7a0b2d332ebb71
vincent <1341870251@qq.com>
2019-11-21 19:19:20 +0800

    1.9

--- src/main/java/com/cool/demo/common/web/AuthController.java
+++ src/main/java/com/cool/demo/common/web/AuthController.java
@@ -2,14 +2,8 @@ package com.cool.demo.common.web;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.cool.demo.common.CodeRes;
-import com.cool.demo.system.entity.Resource;
-import com.cool.demo.system.entity.RoleResource;
-import com.cool.demo.system.entity.User;
-import com.cool.demo.system.entity.UserLogin;
-import com.cool.demo.system.service.ResourceService;
-import com.cool.demo.system.service.RoleResourceService;
-import com.cool.demo.system.service.UserLoginService;
-import com.cool.demo.system.service.UserService;
+import com.cool.demo.system.entity.*;
+import com.cool.demo.system.service.*;
 import com.core.common.Cools;
 import com.core.common.R;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +27,10 @@ public class AuthController extends BaseController {
     private ResourceService resourceService;
     @Autowired
     private RoleResourceService roleResourceService;
+    @Autowired
+    private PermissionService permissionService;
+    @Autowired
+    private RolePermissionService rolePermissionService;
 
     @RequestMapping("/login.action")
     public R loginAction(String mobile, String password){
@@ -102,32 +100,64 @@ public class AuthController extends BaseController {
 
     @RequestMapping("/power/list/auth")
     public R powerList(){
-        List<Resource> oneLevels = resourceService.selectList(new EntityWrapper<Resource>().eq("level", 1).eq("status", 1));
+        List<Resource> oneLevels = resourceService.selectList(new EntityWrapper<Resource>().eq("level", 1).eq("status", 1).orderBy("sort"));
         List<Map> result = new ArrayList<>();
         for (Resource oneLevel : oneLevels){
+            List<Map> twoLevelsList = new ArrayList<>();
             Map<String, Object> oneLevelMap = new HashMap<>();
             oneLevelMap.put("title", oneLevel.getName());
             oneLevelMap.put("id", oneLevel.getCode());
             oneLevelMap.put("spread", true);
-            List<Resource> twoLevels = resourceService.selectList(new EntityWrapper<Resource>().eq("resource_id", oneLevel.getId()).eq("status", 1));
-            List<Map> twoLevelsList = new ArrayList<>();
             oneLevelMap.put("children", twoLevelsList);
+            List<Resource> twoLevels = resourceService.selectList(new EntityWrapper<Resource>().eq("resource_id", oneLevel.getId()).eq("level", 2).eq("status", 1).orderBy("sort"));
+
             for (Resource twoLevel : twoLevels){
+//                List<Map> threeLevelsList = new ArrayList<>();
                 Map<String, Object> twoLevelMap = new HashMap<>();
                 twoLevelMap.put("title", twoLevel.getName());
                 twoLevelMap.put("id", twoLevel.getCode());
-                oneLevelMap.put("spread", true);
+                twoLevelMap.put("spread", true);
+//                twoLevelMap.put("children", threeLevelsList);
+//                List<Permission> permissions = permissionService.selectList(new EntityWrapper<Permission>().eq("resource_id", twoLevel.getId()).eq("status", 1));
+
+//                // 功能操作
+//                for (Permission permission : permissions) {
+//                    Map<String, Object> threeLevel = new HashMap<>();
+//                    threeLevel.put("title", permission.getName().concat("(功能)"));
+//                    threeLevel.put("id", permission.getAction());
+//                    threeLevel.put("spread", true);
+//                    threeLevelsList.add(threeLevel);
+//                }
                 twoLevelsList.add(twoLevelMap);
             }
             result.add(oneLevelMap);
         }
+
+        // 功能模块
+        Map<String, Object> functions = new HashMap<>();
+        functions.put("title", "指定功能");
+        functions.put("id", "function");
+        functions.put("spread", true);
+        List<Map> funcs = new ArrayList<>();
+        functions.put("children", funcs);
+        List<Permission> permissions = permissionService.selectList(new EntityWrapper<Permission>().eq("status", 1));
+        for (Permission permission : permissions) {
+            Map<String, Object> func = new HashMap<>();
+            func.put("title", permission.getName());
+            func.put("id", permission.getAction());
+            func.put("spread", true);
+            funcs.add(func);
+        }
+        result.add(functions);
+
         return R.ok(result);
     }
 
     @RequestMapping(value = "/power/{roleId}/auth")
     public R get(@PathVariable("roleId") Long roleId) {
-        List<RoleResource> roleResources = roleResourceService.selectList(new EntityWrapper<RoleResource>().eq("role_id", roleId));
         List<String> result = new ArrayList<>();
+        // 菜单
+        List<RoleResource> roleResources = roleResourceService.selectList(new EntityWrapper<RoleResource>().eq("role_id", roleId));
         for (RoleResource roleResource : roleResources){
             Resource resource = resourceService.selectById(roleResource.getResourceId());
             if (!Cools.isEmpty(resource)){
@@ -136,19 +166,39 @@ public class AuthController extends BaseController {
                 }
             }
         }
+        // 功能
+        List<RolePermission> rolePermissions = rolePermissionService.selectList(new EntityWrapper<RolePermission>().eq("role_id", roleId));
+        for (RolePermission rolePermission : rolePermissions){
+            Permission permission = permissionService.selectById(rolePermission.getPermissionId());
+            if (!Cools.isEmpty(permission)){
+                result.add(permission.getAction());
+            }
+        }
         return R.ok(result);
     }
 
     @RequestMapping("/power/auth")
     public R power(Long roleId, String[] powers){
         roleResourceService.delete(new EntityWrapper<RoleResource>().eq("role_id", roleId));
+        rolePermissionService.delete(new EntityWrapper<RolePermission>().eq("role_id", roleId));
         if (!Cools.isEmpty(powers)){
             for (String power : powers) {
                 Resource resource = resourceService.selectOne(new EntityWrapper<Resource>().eq("code", power).eq("level", 2));
-                RoleResource roleResource = new RoleResource();
-                roleResource.setRoleId(roleId);
-                roleResource.setResourceId(resource.getId());
-                roleResourceService.insert(roleResource);
+                if (!Cools.isEmpty(resource)) {
+                    RoleResource roleResource = new RoleResource();
+                    roleResource.setRoleId(roleId);
+                    roleResource.setResourceId(resource.getId());
+                    roleResourceService.insert(roleResource);
+                } else {
+                    Permission permission = permissionService.selectOne(new EntityWrapper<Permission>().eq("action", power));
+                    if (!Cools.isEmpty(permission)){
+                        RolePermission rolePermission = new RolePermission();
+                        rolePermission.setRoleId(roleId);
+                        rolePermission.setPermissionId(permission.getId());
+                        rolePermissionService.insert(rolePermission);
+                    }
+                }
+
             }
         }
         return R.ok();
