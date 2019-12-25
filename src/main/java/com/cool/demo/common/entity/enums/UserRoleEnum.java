fa794c8eab4147dccf8cb81f0235e02f217ed521
vincent <1341870251@qq.com>
2019-12-25 14:28:07 +0800

    #

---
+++ src/main/java/com/cool/demo/common/entity/enums/UserRoleEnum.java
@@ -0,0 +1,13 @@
+package com.cool.demo.common.entity.enums;
+
+public enum UserRoleEnum {
+
+    ROOT, // 超级管理员（兼开发人员)
+    ADMIN, // 管理员
+    BOSS, // 安全工作委员会
+    DIRECTOR, // 区域安全负责人
+    LEADER, // 检查组长
+    MEMBER, // 检查组员
+    ;
+
+}
