b6e934c0636d271e8a55a493284970c038d17cb1
vincent <1341870251@qq.com>
2020-03-16 17:05:37 +0800

    #

--- src/main/java/com/cool/demo/common/CodeBuilder.java
+++ src/main/java/com/cool/demo/common/CodeBuilder.java
@@ -12,8 +12,8 @@ public class CodeBuilder {
         generator.url="localhost:3306/plastic";
         generator.username="root";
         generator.password="xltys1995";
-        generator.table="sys_config";
-        generator.packagePath="com.cool.demo.system";
+        generator.table="man_bill";
+        generator.packagePath="com.cool.demo.manager";
         generator.build();
     }
 
