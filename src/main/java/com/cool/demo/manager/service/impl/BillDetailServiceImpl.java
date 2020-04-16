01ff5bcc75d3997543fee6196af7f77a8072b533
lvwenchao <1448963763@qq.com>
2020-04-16 12:21:57 +0800

    装箱

--- src/main/java/com/cool/demo/manager/service/impl/BillDetailServiceImpl.java
+++ src/main/java/com/cool/demo/manager/service/impl/BillDetailServiceImpl.java
@@ -4,9 +4,21 @@ import com.cool.demo.manager.mapper.BillDetailMapper;
 import com.cool.demo.manager.entity.BillDetail;
 import com.cool.demo.manager.service.BillDetailService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("billDetailService")
 public class BillDetailServiceImpl extends ServiceImpl<BillDetailMapper, BillDetail> implements BillDetailService {
 
+    @Autowired
+    private  BillDetailMapper billDetailMapper;
+    @Override
+    public BillDetail getBillDetailByOrderIdandBoxNumber(Long billId, Long boxNumber ) {
+        return billDetailMapper.getBillDetailByOrderIdandBoxNumber(billId,boxNumber );
+    }
+
+    @Override
+    public int getStatistics(String boxer) {
+        return billDetailMapper.getStatistics(boxer);
+    }
 }
