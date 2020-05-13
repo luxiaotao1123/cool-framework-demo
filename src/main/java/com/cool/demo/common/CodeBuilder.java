62a3ef6ce43121f70d3ffbf6cad57492785b0926
vincent <1341870251@qq.com>
2020-05-13 12:49:53 +0800

    #

--- src/main/java/com/cool/demo/common/CodeBuilder.java
+++ src/main/java/com/cool/demo/common/CodeBuilder.java
@@ -9,11 +9,11 @@ public class CodeBuilder {
 
     public static void main(String[] args) throws Exception {
         CoolGenerator generator = new CoolGenerator();
-        generator.url="localhost:3306/cool";
-        generator.username="root";
-        generator.password="xltys1995";
-        generator.table="sys_operate_log";
-        generator.packagePath="com.cool.demo.system";
+        generator.url="192.168.3.211:1433;databasename=cool";
+        generator.username="sa";
+        generator.password="sa@123";
+        generator.table="asr_loc_detl";
+        generator.packagePath="com.cool.demo.manager";
         generator.build();
     }
 
