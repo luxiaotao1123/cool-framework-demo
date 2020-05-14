9f8c19851b34e6f69d341181bb4cf2b244d40236
vincent <1341870251@qq.com>
2020-05-14 08:12:09 +0800

    #

--- src/main/java/com/cool/demo/common/CodeBuilder.java
+++ src/main/java/com/cool/demo/common/CodeBuilder.java
@@ -10,16 +10,16 @@ public class CodeBuilder {
     public static void main(String[] args) throws Exception {
         CoolGenerator generator = new CoolGenerator();
         // mysql
-        generator.url="localhost:3306/cool";
-        generator.username="root";
-        generator.password="xltys1995";
-        generator.table="sys_role";
+//        generator.url="localhost:3306/cool";
+//        generator.username="root";
+//        generator.password="xltys1995";
+//        generator.table="sys_role";
         // sqlserver
-//        generator.url="192.168.3.211:1433;databasename=cool";
-//        generator.username="sa";
-//        generator.password="sa@123";
-//        generator.table="asr_loc_detl";
-        generator.packagePath="com.cool.demo.system";
+        generator.url="192.168.3.211:1433;databasename=cool";
+        generator.username="sa";
+        generator.password="sa@123";
+        generator.table="asr_bas_wrk_status";
+        generator.packagePath="com.cool.demo.manager";
         generator.build();
     }
 
