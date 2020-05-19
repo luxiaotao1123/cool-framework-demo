a3a2c9e6ab22815154358aea104a414be38fe446
vincent <1341870251@qq.com>
2020-05-19 09:32:35 +0800

    #

--- src/main/java/com/cool/demo/common/CodeBuilder.java
+++ src/main/java/com/cool/demo/common/CodeBuilder.java
@@ -18,7 +18,7 @@ public class CodeBuilder {
         generator.url="192.168.1.100:1433;databasename=cool";
         generator.username="sa";
         generator.password="sa@123";
-        generator.table="asr_bas_crnp";
+        generator.table="asr_wrk_mast";
         generator.packagePath="com.cool.demo.asrs";
         generator.build();
     }
