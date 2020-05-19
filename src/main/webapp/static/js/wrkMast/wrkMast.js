e4142b41adbc9b1e7bd9681e93fd631d57e876fe
vincent <1341870251@qq.com>
2020-05-19 10:22:37 +0800

    #

--- src/main/webapp/static/js/wrkMast/wrkMast.js
+++ src/main/webapp/static/js/wrkMast/wrkMast.js
@@ -12,7 +12,7 @@ layui.use(['table','laydate', 'form'], function(){
         headers: {token: localStorage.getItem('token')},
         url: '/wrkMast/list/auth',
         page: true,
-        limit: 16,
+        limit: 5,
         even: true,
         toolbar: '#toolbar',
         cellMinWidth: 50,
