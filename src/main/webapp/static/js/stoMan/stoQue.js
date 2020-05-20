766d1164862f162c47cdac6decba6055dbe54793
vincent <1341870251@qq.com>
2020-05-20 14:25:14 +0800

    #

---
+++ src/main/webapp/static/js/stoMan/stoQue.js
@@ -0,0 +1,174 @@
+var pageCurr;
+layui.use(['table','laydate', 'form'], function(){
+    var table = layui.table;
+    var $ = layui.jquery;
+    var layer = layui.layer;
+    var layDate = layui.laydate;
+    var form = layui.form;
+
+
+    // 数据渲染
+    tableIns = table.render({
+        elem: '#stoQue',
+        headers: {token: localStorage.getItem('token')},
+        url: '/locMast/list/auth',
+        page: true,
+        limit: 5,
+        even: true,
+        toolbar: '#toolbar',
+        cellMinWidth: 50,
+        cols: [[
+            {type: 'checkbox', fixed: 'left'}
+            ,{field: 'locNo', align: 'center',title: '库位号'}
+            ,{field: 'locType$', align: 'center',title: '库位状态',event: 'locType', style: 'text-decoration: underline;cursor:pointer',width:200}
+            ,{field: 'whsType$', align: 'center',title: '库位类型',event: 'whsType', style: 'text-decoration: underline;cursor:pointer'}
+            ,{field: 'crnNo', align: 'center',title: '堆垛机号'}
+            ,{field: 'row1', align: 'center',title: '排', width:80}
+            ,{field: 'bay1', align: 'center',title: '列', width:80}
+            ,{field: 'lev1', align: 'center',title: '层', width:80}
+            ,{field: 'fullPlt', align: 'center',title: '满板', templet:function(row){
+                    var html = "<input value='fullPlt' type='checkbox' lay-skin='primary' lay-filter='tableCheckbox' table-index='"+row.LAY_TABLE_INDEX+"'";
+                    if(row.fullPlt === 'Y'){html += " checked ";}
+                    html += ">";
+                    return html;
+                },width:80}
+            ,{field: 'modiUser$', align: 'center',title: '修改人员',event: 'modiUser', style: 'cursor:pointer'}
+            ,{field: 'modiTime$', align: 'center',title: '修改时间', width:200}
+            ,{ fixed: 'right', title:'操作', align: 'center', toolbar: '#operate', width:150}
+        ]],
+        request: {
+            pageName: 'curr',
+            pageSize: 'limit'
+        },
+        parseData: function (res) {
+            return {
+                'code': res.code,
+                'msg': res.msg,
+                'count': res.data.total,
+                'data': res.data.records
+            }
+        },
+        response: {
+            statusCode: 200
+        },
+        done: function(res, curr, count) {
+            if (res.code === 403) {
+                top.location.href = "/";
+            }
+            pageCurr=curr;
+            limit();
+            form.on('checkbox(tableCheckbox)', function (data) {
+                var _index = $(data.elem).attr('table-index')||0;
+                if(data.elem.checked){
+                    res.data[_index][data.value] = 'Y';
+                }else{
+                    res.data[_index][data.value] = 'N';
+                }
+            });
+        }
+    });
+
+    // 监听行工具事件
+    table.on('tool(locMast)', function(obj) {
+        var data = obj.data;
+        switch (obj.event) {
+            // 更新库存
+            case 'refreshSto':
+                layer.open({
+                    type: 2,
+                    title: '更新库存',
+                    maxmin: true,
+                    area: [top.detailWidth, top.detailHeight],
+                    shadeClose: false,
+                    content: 'locMast_detail.html',
+                    success: function (layero, index) {
+                        // todo 更新库存
+                    }
+                });
+                break;
+        }
+    });
+
+    // 搜索栏搜索事件
+    form.on('submit(search)', function (data) {
+        pageCurr = 1;
+        tableReload(false);
+    });
+});
+
+// 关闭动作
+$(document).on('click','#data-detail-close', function () {
+    parent.layer.closeAll();
+});
+
+function tableReload(child) {
+    var searchData = {};
+    $.each($('#search-box [name]').serializeArray(), function() {
+        searchData[this.name] = this.value;
+    });
+    (child ? parent.tableIns : tableIns).reload({
+        where: searchData,
+        page: {
+            curr: pageCurr
+        },
+        done: function (res, curr, count) {
+            if (res.code === 403) {
+                top.location.href = "/";
+            }
+            pageCurr=curr;
+            if (res.data.length === 0 && count !== 0) {
+                tableIns.reload({
+                    where: searchData,
+                    page: {
+                        curr: pageCurr-1
+                    }
+                });
+                pageCurr -= 1;
+            }
+            limit(child);
+        }
+    });
+}
+
+function setFormVal(el, data, showImg) {
+    for (var val in data) {
+        var find = el.find(":input[id='" + val + "']");
+        find.val(data[val]);
+        if (showImg){
+            var next = find.next();
+            if (next.get(0)){
+                if (next.get(0).localName === "img") {
+                    find.hide();
+                    next.attr("src", data[val]);
+                    next.show();
+                }
+            }
+        }
+    }
+}
+
+function clearFormVal(el) {
+    $(':input', el)
+        .val('')
+        .removeAttr('checked')
+        .removeAttr('selected');
+}
+
+function detailScreen(index) {
+    var detail = layer.getChildFrame('#data-detail', index);
+    var height = detail.height()+60;
+    if (height > ($(window).height()*0.9)) {
+        height = ($(window).height()*0.9);
+    }
+    layer.style(index, {
+        top: (($(window).height()-height)/3)+"px",
+        height: height+'px'
+    });
+    $(".layui-layer-shade").remove();
+}
+
+$('body').keydown(function () {
+    if (event.keyCode === 13) {
+        $("#search").click();
+    }
+});
