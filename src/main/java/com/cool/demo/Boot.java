408e079281fd5742b8b8595d404b0bf4ac8cb38c
vincent <1341870251@qq.com>
2020-05-24 21:13:19 +0800

    #

--- src/main/java/com/cool/demo/Boot.java
+++ src/main/java/com/cool/demo/Boot.java
@@ -2,12 +2,19 @@ package com.cool.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Boot {
+public class Boot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Boot.class);
     }
 
 }
