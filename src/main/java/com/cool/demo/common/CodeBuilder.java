05f2832f35b0acafc75d5380a1d1ed80286e699f
luxiaotao1123 <t1341870251@163.com>
2020-08-29 14:03:38 +0800

    #

--- src/main/java/com/cool/demo/common/CodeBuilder.java
+++ src/main/java/com/cool/demo/common/CodeBuilder.java
@@ -10,16 +10,16 @@ public class CodeBuilder {
     public static void main(String[] args) throws Exception {
         CoolGenerator generator = new CoolGenerator();
         // mysql
-//        generator.url="localhost:3306/cool";
-//        generator.username="root";
-//        generator.password="xltys1995";
-//        generator.table="sys_host";
+        generator.url="localhost:3306/db_xty";
+        generator.username="root";
+        generator.password="root";
+        generator.table="sys_host";
         // sqlserver
-        generator.url="127.0.0.1:1433;databasename=yfzx";
-        generator.username="sa";
-        generator.password="sa@123";
-        generator.table="man_wp_detl";
-        generator.packagePath="com.cool.demo.manager";
+//        generator.url="127.0.0.1:1433;databasename=db_xty";
+//        generator.username="sa";
+//        generator.password="sa@123";
+//        generator.table="man_wp_detl";
+//        generator.packagePath="com.cool.demo.manager";
         generator.js = false;
         generator.html = false;
         generator.htmlDetail = false;
