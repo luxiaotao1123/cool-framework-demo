96fad86b6a904664e9a7ac3b5c51fd84ee919ab3
luxiaotao1123 <t1341870251@163.com>
2020-10-12 08:47:01 +0800

    #

--- src/main/java/com/cool/demo/Boot.java
+++ src/main/java/com/cool/demo/Boot.java
@@ -1,13 +1,20 @@
 package com.cool.demo;
 
+import com.cool.demo.common.utils.RandomValidateCodeUtil;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Boot {
+public class Boot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        RandomValidateCodeUtil.init();
         SpringApplication.run(Boot.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        RandomValidateCodeUtil.init();
+        return builder.sources(Boot.class);
+    }
 }
